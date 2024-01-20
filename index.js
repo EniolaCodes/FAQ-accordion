f5f283ecd0b62ad8a197bc34ec31e4f536d08c8e
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-01-20 23:44:04 +0100

    fix the toggle functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let i;
 
 for(i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", function(){
-        this.classList.toggle = ("active");
+        this.classList.toggle("active");
         let answer = this.nextElementSibling;
         if(answer.style.maxHeight){
             answer.style.maxHeight = null;
