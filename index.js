2ec7e95e7aba57f3a94034910d4546c62eb12458
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-01-20 19:51:01 +0100

    add the functionalities

---
+++ index.js
@@ -0,0 +1,22 @@
+let accordion = document.getElementsByClassName("accordionWrapper");
+let i;
+
+for(i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener("click", function(){
+        this.classList.toggle = ("active");
+        let answer = this.nextElementSibling;
+        if(answer.style.maxHeight){
+            answer.style.maxHeight = null;
+        } else{
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+    })
+        
+    
+}
+
+
+
+
+
+
